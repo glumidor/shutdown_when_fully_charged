6a35090c4696bb1f65746e42bdeb5d83da362880
Fr@nk <glumidor@users.noreply.github.com>
2022-03-12 11:45:20 +0100

    Update shutdown_when_fully_charged.sh
    added variables
    breaking up long lines
    for human readability

--- shutdown_when_fully_charged.sh
+++ shutdown_when_fully_charged.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+loop_time=60 # secs
+battery_full_sound="/System/Library/Sounds/Glass.aiff"
+shutdown_sound="/System/Library/Sounds/Submarine.aiff"
+
 while true; do
-	clear
-	echo
-	echo
+	clear; echo; echo
 	pmset -g batt 
 	echo
-	percent=$(pmset -g batt | grep "InternalBattery" | awk -F ";" '{print $1}' | awk -F " " '{print $3}' | sed "s/%//g" | sed "s/\s//g")
-	remaining_hour=$(pmset -g batt | grep "InternalBattery" |  awk -F ";" '{print $3}' | awk -F " " '{print $1}' | awk -F ":" '{print $1}' | sed "s/\s//g")
-	remaining_min=$(pmset -g batt | grep "InternalBattery" |  awk -F ";" '{print $3}' | awk -F " " '{print $1}' | awk -F ":" '{print $2}' | sed "s/\s//g")
+	
+	percent=$(pmset -g batt 		|\
+			grep "InternalBattery"  |\
+			awk -F ";" '{print $1}' |\
+			awk -F " " '{print $3}' |\
+			sed "s/%//g" 		|\
+			sed "s/\s//g")
+
+	remaining_hour=$(pmset -g batt 		|\
+			grep "InternalBattery"  |\
+			awk -F ";" '{print $3}' |\
+			awk -F " " '{print $1}' |\
+			awk -F ":" '{print $1}' |\
+			sed "s/\s+//g")
+
+	remaining_min=$(pmset -g batt 		|\
+			grep "InternalBattery"  |\
+			awk -F ";" '{print $3}' |\
+			awk -F " " '{print $1}' |\
+			awk -F ":" '{print $2}' |\
+			sed "s/\s//g")			
+
 	remaining=$(expr $remaining_hour \* 60 + $remaining_min)
+
 	echo
 	echo $percent "% charged"
 	echo $remaining "min remaining"
@@ -17,12 +39,26 @@ while true; do
 
 	if [ $percent -gt 99 ] 
 		then 
-			afplay /System/Library/Sounds/Glass.aiff
+			afplay $battery_full_sound
 
 			pmset -g batt
 
-			remaining_hour=$(pmset -g batt | grep "InternalBattery" | grep "finishing charge" | awk -F ";" '{print $3}' | awk -F " " '{print $1}' | awk -F ":" '{print $1}' | sed "s/\s//g")
-			remaining_min=$(pmset -g batt | grep "InternalBattery" |  grep "finishing charge" | awk -F ";" '{print $3}' | awk -F " " '{print $1}' | awk -F ":" '{print $2}' | sed "s/\s//g")
+			remaining_hour=$(pmset -g batt  |\
+				grep "InternalBattery"  |\
+				grep "finishing charge" |\
+				awk -F ";" '{print $3}' |\
+				awk -F " " '{print $1}' |\
+				awk -F ":" '{print $1}' |\
+				sed "s/\s//g")
+
+			remaining_min=$(pmset -g batt   |\
+				grep "InternalBattery"  |\
+				grep "finishing charge" |\
+				awk -F ";" '{print $3}' |\
+				awk -F " " '{print $1}' |\
+				awk -F ":" '{print $2}' |\
+				sed "s/\s//g")
+
 			remaining=$(expr $remaining_hour \* 60 + $remaining_min 2>/dev/null)
 			
 			restzeit=$(expr $remaining + 5 2>/dev/null) # min
@@ -34,15 +70,15 @@ while true; do
 				pmset -g batt
 				echo
 				echo "shutdown in $restzeit min..."
-				sleep 60
+				sleep $loop_time
 				((restzeit--))
 			done
 
-			afplay /System/Library/Sounds/Submarine.aiff
+			afplay $shutdown_sound
 			sleep 10
-			sudo shutdown -h now
+			sudo shutdown -h now  # make sure your user has sudo-nopassword-option set
 			exit
 	fi
 	
-	sleep 60
+	sleep $loop_time
 done
